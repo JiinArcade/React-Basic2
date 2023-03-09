afcdb33434e03f69e9f2096b4ebea4d5cececfed
JiinArcade <jn_ll@naver.com>
2023-03-09 11:27:29 +0900

    💄 스타일링 시작

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,8 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Index from "./javabasic/Index.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
 }
 
 export default App;
+
+//받아오기
---
+++ reactbasic/src/javabasic/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="wrapper">
+      <h1 className="logo">
+        <a href="./Index.jsx"></a>
+      </h1>
+    </div>
+  );
+}
+
+function Body() {
+  return <div>dfafasfa</div>;
+}
+
+function Fotter() {
+  return <div>dfafas53453454fa</div>;
+}
+
+function Index() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Fotter />
+    </div>
+  );
+}
+
+export default Index;
+
+//수입수출

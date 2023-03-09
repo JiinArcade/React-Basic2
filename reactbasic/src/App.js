0024294ef14944261861ed57b0d263d86fecbdd9
JiinArcade <jn_ll@naver.com>
2023-03-09 12:20:20 +0900

    💄 스타일링 중

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Index from "./javabasic/Index.jsx";
 
 function App() {
   return (
     <div className="App">
+      
       <Index />
     </div>
   );
--- reactbasic/src/javabasic/index.jsx
+++ reactbasic/src/javabasic/index.jsx
@@ -1,21 +1,95 @@
 import React from "react";
+import logo from "./style.css";
 
 function Header() {
   return (
-    <div className="wrapper">
-      <h1 className="logo">
-        <a href="./Index.jsx"></a>
-      </h1>
-    </div>
+    <header>
+      <nav>
+        <h1 className="logo">
+          <a href="./">
+            <img className="logo" src={logo} alt="logo" />
+          </a>
+        </h1>
+        <ul className="gnbList">
+          <li className="gnbItem">
+            <a href="./">menu1</a>
+          </li>
+          <li className="gnbItem">
+            <a href="./">menu2</a>
+          </li>
+          <li className="gnbItem">
+            <a href="./">menu3</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
 function Body() {
-  return <div>dfafasfa</div>;
+  return (
+    <div className="bodyWrapper">
+      <section className="backgroundImge"></section>
+      <h1>Lorem ipsum dolor sit.</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem in id
+        nemo eum, iure illum!
+      </p>
+      <button type="button">버튼</button>
+      {/* onClick={alert("hi")} */}
+
+      <div className="top">
+        <h3>Lorem ipsum dolor sit amet.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+          omnis modi? Cupiditate totam sed a atque sint blanditiis minima cum?
+        </p>
+      </div>
+
+      <section className="postBox">
+        <div className="postCard">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p className="postBoxTxt">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+            omnis modi? Cupiditate totam sed a atque sint blanditiis minima cum?
+          </p>
+        </div>
+        <div className="postCard">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p className="postBoxTxt">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+            omnis modi? Cupiditate totam sed a atque sint blanditiis minima cum?
+          </p>
+        </div>
+        <div className="postCard">
+          <h3>Lorem ipsum dolor sit amet.</h3>
+          <p className="postBoxTxt">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+            omnis modi? Cupiditate totam sed a atque sint blanditiis minima cum?
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 function Fotter() {
-  return <div>dfafas53453454fa</div>;
+  return (
+    <div class="global-footer">
+      <h3>Contact</h3>
+      <ul class="footer-list">
+        <li class="footer-item">
+          <a href="jn_ll@naver.com">메일</a>
+        </li>
+        <li class="footer-item">
+          <a href="tel:01082091094">전화</a>
+        </li>
+        <li class="footer-item">
+          <a href="https://www.youtube.com">유튜브</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function Index() {

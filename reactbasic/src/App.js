82c662e61244ac7a4d60ced63f9d4a42d6dc1383
JiinArcade <jn_ll@naver.com>
2023-03-15 11:46:13 +0900

    ✅ Footer 작성

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 // import Index from "./javabasic/Index.jsx";
 import Header from "./component/Header/Header";
 import Main from "./component/Main/Main"
+import Cards from "./component/Main/Cards";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Main></Main>
+      <Cards></Cards>
     </div>
   );
 }
---
+++ reactbasic/src/component/Main/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Cards.css'
+
+const Cards = () => {
+  return (
+    <section class="post-box">
+      <div class="post-card">
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p class="post-card-txt">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sint aperiam tempora ratione sunt beatae alias nulla<br />
+          unde expedita suscipit quisquam natus dolores aliquam, placeat ea iusto, nesciunt, incidunt minima.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Cards

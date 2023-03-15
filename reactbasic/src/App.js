28283320bd44dc3522f6c7827c6fcc0001b3c269
JiinArcade <jn_ll@naver.com>
2023-03-15 11:30:38 +0900

    💄 스타일링

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 // import Index from "./javabasic/Index.jsx";
 import Header from "./component/Header/Header";
+import Main from "./component/Main/Main"
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Main></Main>
     </div>
   );
 }
---
+++ reactbasic/src/component/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <main>
+      <Ipsum layout='background' name='front-txt' />
+      <Ipsum layout='top' name='second-txt' />
+    </main>
+  )
+}
+
+function Ipsum(props) {
+  const showBtn = props.name === 'front-txt' ? true : false
+  return (
+    <section className={props.layout}>
+      <div className={props.name}>
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi sint aperiam tempora ratione sunt beatae alias nulla<br />
+          unde expedita suscipit quisquam natus dolores aliquam, placeat ea iusto, nesciunt, incidunt minima.
+        </p>
+        {
+          // showBtn이 true 일 때만 button을 렌더링
+          // 둘 다 true 일 경우에만 버튼을 렌더링 하고
+          // 좌항이 false일 경우 바로 false를 반환한다.
+          showBtn && <button className='front-btn'>버튼</button>
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Main
